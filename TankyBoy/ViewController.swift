432173bc85e3deca65f6e3ba0720f824af7ea5d0
Alexander Snitko <asyncamisu@gmail.com>
2022-05-04 00:42:02 +0300

    button actions added

--- TankyBoy/ViewController.swift
+++ TankyBoy/ViewController.swift
@@ -10,15 +10,27 @@ import RealityKit
 
 class ViewController: UIViewController {
     
+    var tankAnchor: TinyToyTank._TinyToyTank?
+    
     @IBOutlet var arView: ARView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tankAnchor = try! TinyToyTank.load_TinyToyTank()
+        arView.scene.anchors.append(tankAnchor!)
+    }
+    
+    @IBAction func turretLeftPressed(_ sender: Any) {
+    }
+    @IBAction func turretRightPressed(_ sender: Any) {
+    }
+    @IBAction func cannonFirePressed(_ sender: Any) {
+    }
+    @IBAction func tankLeftPressed(_ sender: Any) {
+    }
+    @IBAction func tankForwardPressed(_ sender: Any) {
+    }
+    @IBAction func tankRightPressed(_ sender: Any) {
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
     }
 }
