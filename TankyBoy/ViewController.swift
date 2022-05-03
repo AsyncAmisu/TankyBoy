a77489ee3f1b64c6c603d0fc049a07a687818b97
Alexander Snitko <asyncamisu@gmail.com>
2022-05-04 00:57:01 +0300

    preventing actions added

--- TankyBoy/ViewController.swift
+++ TankyBoy/ViewController.swift
@@ -11,26 +11,53 @@ import RealityKit
 class ViewController: UIViewController {
     
     var tankAnchor: TinyToyTank._TinyToyTank?
+    var isActionPlaying: Bool = false
     
     @IBOutlet var arView: ARView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tankAnchor = try! TinyToyTank.load_TinyToyTank()
+        tankAnchor!.cannon?.setParent(tankAnchor!.tank, preservingWorldTransform: true)
+        tankAnchor?.actions.actionComplete.onAction = { _ in
+            self.isActionPlaying = false
+        }
         arView.scene.anchors.append(tankAnchor!)
     }
     
     @IBAction func turretLeftPressed(_ sender: Any) {
+        if self.isActionPlaying { return }
+        else { self.isActionPlaying = true }
+        tankAnchor!.notifications.turretLeft.post()
     }
+    
     @IBAction func turretRightPressed(_ sender: Any) {
+        if self.isActionPlaying { return }
+        else { self.isActionPlaying = true }
+        tankAnchor!.notifications.turretRight.post()
     }
+    
     @IBAction func cannonFirePressed(_ sender: Any) {
+        if self.isActionPlaying { return }
+        else { self.isActionPlaying = true }
+        tankAnchor!.notifications.cannonFire.post()
     }
+    
     @IBAction func tankLeftPressed(_ sender: Any) {
+        if self.isActionPlaying { return }
+        else { self.isActionPlaying = true }
+        tankAnchor!.notifications.tankLeft.post()
     }
+    
     @IBAction func tankForwardPressed(_ sender: Any) {
+        if self.isActionPlaying { return }
+        else { self.isActionPlaying = true }
+        tankAnchor!.notifications.tankForward.post()
     }
+    
     @IBAction func tankRightPressed(_ sender: Any) {
-        
+        if self.isActionPlaying { return }
+        else { self.isActionPlaying = true }
+        tankAnchor!.notifications.tankRight.post()
     }
 }
